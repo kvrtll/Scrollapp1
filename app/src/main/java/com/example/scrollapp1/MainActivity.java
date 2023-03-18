37284d0e673cf6010092a571ad76c70e4825d0cc
python <shishik16198@gmail.com>
2023-03-18 09:46:57 +0300

    18.03.2023 1.2

---
+++ app/src/main/java/com/example/scrollapp1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.scrollapp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView plList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        plList = findViewById(R.id.tv_pl_names);
+
+        String[] names = {"H","e","l","l","o","","w","o","r","l","d","","my","name","is","Nikita","^_^"};
+
+        plList.setText("");
+        for(String name : names) {
+            plList.append(name+ "\n");
+        }
+
+
+    }
+}
